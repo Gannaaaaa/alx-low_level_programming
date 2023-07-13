087cd366ff60c0088fc1ee3a77368a3a82e75d8d
gannaaaaa <ganawesam82@gmail.com>
2023-07-13 11:34:20 -0700

    first 3 tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 /**
  * main - A program that prints a line with puts function
  * Return: 0 (Success)
- */
+*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
