d67fb304baca9e34ce490d231a2a986021433c78
gannaaaaa <ganawesam82@gmail.com>
2023-07-19 10:40:34 -0700

    tasks

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,21 @@
 #include "main.h"
-
 /**
  * main - print _putchar
  * is written by mbah
  * Return: 0
-*/
+ */
+
 int main(void)
 {
-	char word[] = "_putchar";
-	int c;
-
-	for (c = 0; c < 8; c++)
-
-	{
-		_putchar(word[c]); }
-	_putchar('\n');
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
+
