d4cae8eca1890cf21a155245c211a3bc6711b93d
gannaaaaa <ganawesam82@gmail.com>
2023-07-19 11:10:01 -0700

    tasks

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include "main.h";
+
+/**
+ * print_alphabet - print all alphabet in lowercase
+*/
+
+
+void print_alphabet(void);
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+
+	return (0);
+
+
+
+
+
+
+
+
+}
+
