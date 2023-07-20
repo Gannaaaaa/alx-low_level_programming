695a5ddc76d758a3fc83dbc5bfb289c44773dc97
gannaaaaa <ganawesam82@gmail.com>
2023-07-20 13:16:34 -0700

    tasks

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *more_numbers - print 0 - 14 ten times
+ *only using -putchar three times
+ *Return: Always 0 (Success)
+*/
+
+void more_numbers(void)
+{
+	int i, nums, temp;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (nums = 0; nums < 15; nums++)
+		{
+			temp = nums;
+			if (nums > 9)
+			{
+				_putchar(1 + 48);
+				temp = nums % 10;
+			}
+			_putchar(temp + 48);
+		}
+			_putchar('\n');
+	}
+
+
+
+
+}
