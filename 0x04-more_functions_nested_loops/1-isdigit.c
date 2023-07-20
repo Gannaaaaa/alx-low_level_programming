7be04ca2916982cf022fae81ad47e1b6efb236c7
gannaaaaa <ganawesam82@gmail.com>
2023-07-20 11:55:06 -0700

    tasks

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
-/** 
- * _isdigit - checks if input is digit between 0 -9
+/**
+ * _isdigit - checks if input is digit between 0 - 9
  * @c: input
  * Return: 1 if is digit, 0 if not
 */
