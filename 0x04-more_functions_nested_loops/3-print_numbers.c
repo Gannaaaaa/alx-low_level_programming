30e06e2de093d0fc1e297bed8c47ce61714ba58c
gannaaaaa <ganawesam82@gmail.com>
2023-07-20 12:15:46 -0700

    tasks

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *for loop to printnumbers - print 0 - 9
+ *only using -putchar twice
+ *Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		_putchar(i);
+	_putchar('\n');
+
+
+
+
+}
