5843c659ae949e410beb84671cbf8a7ac100a3e6
gannaaaaa <ganawesam82@gmail.com>
2023-07-25 13:37:48 -0700

    tasks

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints reversed string, followed by a new line
+ * @s: pointer to the string to print
+ * Return: void
+*/
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (; *s != '\0'; s++)
+		i++;
+	for (; i >= 0; i--)
+	{
+		_putchar(s[i]);
+
+	}
+	_putchar('\n');
+}
