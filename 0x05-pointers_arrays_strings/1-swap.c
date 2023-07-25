f05be3464a39cdf41dce872d80df150008031f57
gannaaaaa <ganawesam82@gmail.com>
2023-07-25 11:47:14 -0700

    tasks

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
 /**
- * swap_int swaps the valuas of to integers
+ * swap_int - swaps the valuas of to integers
  * using two input parameters
+ *
  * @a: input paramter 1
  * @b: input paramter 2
- * return: Nothing
+ *
+ * Return: Nothing
 */
 
 void swap_int(int *a, int *b)
