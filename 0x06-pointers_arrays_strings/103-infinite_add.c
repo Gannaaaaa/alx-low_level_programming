a6906b0ecb2c8794c0da2cf01f7c02f70d638b2f
gannaaaaa <ganawesam82@gmail.com>
2023-07-27 13:04:38 -0700

    tasks

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -10,4 +10,42 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
+	int i = 0, j = 0, d = 0, f, s, l;
 
+	while (n1[i] != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	if (i > j)
+		l = i;
+	else
+		l = j;
+	if (l + 1 > size_r)
+		return (0);
+	r[l] = '\0';
+	for (l -= l; l >= 0; l--)
+	{
+		i--;
+		j--;
+		if (i >= 0)
+			f = n1[i] - 48;
+		else
+			f = 0;
+		if (j >= 0)
+			s = n2[j] - 48;
+		else
+			s = 0;
+		r[l] = (f + s + d) % 10 + '\0';
+		d = (f + s + d) / 10;
+	}
+	if (d == 1)
+	{
+		if (l + 2 > size_r)
+			return (0);
+		r[l + 1] = '\0';
+		while (l-- > 0)
+			r[l + 1] = r[l];
+		r[0] = '1';
+	}
+	return (r);
+}
