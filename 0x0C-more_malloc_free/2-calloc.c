799304b4aae9b6c4ff75ab04a746fe2726b3e67c
gannaaaaa <ganawesam82@gmail.com>
2023-08-10 14:49:59 -0700

    tasks

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -31,7 +31,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	p = malloc(nmemb * size);
+	p = malloc(size * nmemb);
 	if (!p)
 		return (0);
 	_memset(p, 0, nmemb * size);
