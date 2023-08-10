2472bfbf5cd4c67130a32a7743436cfe75d847f0
gannaaaaa <ganawesam82@gmail.com>
2023-08-10 15:17:10 -0700

    tasks

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,12 +14,12 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (0);
-	while (min <= (max + 1))
+	while (min++ <= max)
 		i++;
 	p = malloc(4 * i);
 	if (!p)
 		return (0);
-	for (j = 0; j <= i; j++)
+	for (j = 0; j < i; j++)
 		p[j] = min++;
 	return (p);
 }
