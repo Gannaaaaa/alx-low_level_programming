022d5d942f15db08b0cf0d29bf998f2e26757a23
gannaaaaa <ganawesam82@gmail.com>
2023-08-16 11:51:22 -0700

    tasks

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,10 +8,10 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	if (array == 0 || size <= 0 || cmp == 0)
-		return (-1);
 	int i;
 
+	if (array == 0 || size <= 0 || cmp == 0)
+		return (-1);
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
