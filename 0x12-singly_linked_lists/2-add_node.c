a4bfa3226a867c714bfeaa5e0005d0bf40a28816
gannaaaaa <ganawesam82@gmail.com>
2023-08-25 19:10:00 -0700

    tasks

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,9 +12,9 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new = malloc(sizeof(list_t));
-	 unsigned int len = 0;
+	unsigned int len = 0;
 
-	 while (str[len])
+	while (str[len])
 		len++;
 	if (!new || !head)
 		return (NULL);
@@ -28,7 +28,7 @@ list_t *add_node(list_t **head, const char *str)
 		}
 		new->len = len;
 	}
-	new->next  = head;
+	new->next  = *head;
 	*head = new;
 	return (new);
 }
